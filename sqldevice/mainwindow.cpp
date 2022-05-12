c353645a88f1615f1fd555e4dea6dabf29eb71c7
Ghost <1715352166@qq.com>
2022-05-12 22:53:12 +0800

    添加界面切换功能

--- sqldevice/mainwindow.cpp
+++ sqldevice/mainwindow.cpp
@@ -1,18 +1,45 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
 #include <QFile>
+#include <QListWidget>
 #include <QDebug>
+
+int first_index;
 
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent)
     , ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
+
+    mqtt_win = new mqttview(this);
+    sql_win = new sqlserver(this);
+
+    ui->stackedWidget->addWidget(mqtt_win);
+    ui->stackedWidget->addWidget(sql_win);
+    ui->stackedWidget->setCurrentWidget(mqtt_win);
+
+    first_index = ui->stackedWidget->currentIndex();
+    qDebug() << "first index " << first_index;
+    //listwidget 添加项目
+    QListWidgetItem *item = new QListWidgetItem("mqtt");
+    //itemlist.append(item);                      //加入链表，后期释放
+    item->setTextAlignment(Qt::AlignHCenter);   //设置文字水平居中
+    ui->listWidget->addItem(item);             //把居中后的item加到ListWidget中
+    QListWidgetItem *item2 = new QListWidgetItem("sql");
+    //itemlist.append(item);                      //加入链表，后期释放
+    item2->setTextAlignment(Qt::AlignHCenter);   //设置文字水平居中
+    ui->listWidget->addItem(item2);             //把居中后的item加到ListWidget中
+    connect(ui->listWidget,SIGNAL(currentRowChanged(int)),this,SLOT(slotRowChanged(int)));
+
+
 }
 
 MainWindow::~MainWindow()
 {
     delete ui;
+    delete mqtt_win;
+    delete sql_win;
 }
 
 #if 1
@@ -41,4 +68,11 @@ void MainWindow::mousePressEvent(QMouseEvent *event)
     i++;
 }
 #endif
+
+void MainWindow::slotRowChanged(int currentRow)
+{
+    qDebug() << currentRow;
+    ui->stackedWidget->setCurrentIndex(currentRow+2);
+}
+
 
--- sqldevice/mainwindow.h
+++ sqldevice/mainwindow.h
@@ -2,6 +2,8 @@
 #define MAINWINDOW_H
 
 #include <QMainWindow>
+#include "mqttview.h"
+#include "sqlserver.h"
 
 QT_BEGIN_NAMESPACE
 namespace Ui { class MainWindow; }
@@ -16,8 +18,12 @@ public:
     ~MainWindow();
 
     void mousePressEvent(QMouseEvent *event);
+public slots:
+    void slotRowChanged(int currentRow);
 
 private:
     Ui::MainWindow *ui;
+    mqttview *mqtt_win;
+    sqlserver *sql_win;
 };
 #endif // MAINWINDOW_H
